761e375c635914a4568e7115721669a45dc572a7
Luis H. Matos <ReachLuis@Gmail.com>
2016-10-12 18:44:00 -0500

    Dashboard Personal Stats Box Step/Undo buttons functional

--- public/scripts/controllers/dashController.js
+++ public/scripts/controllers/dashController.js
@@ -3,30 +3,53 @@ myApp.controller('dashController', ['$scope', '$http', function($scope, $http){
   //// Global variables
   // for testing - to be replaced by Auth0 logic
   var loggedIn = true;
-  $scope.stepDone = true;
+  $scope.stepDone = false;
 
   $scope.getMember = function(){
-    console.log( 'in test post' );
+    console.log( 'in getMember()' );
+
+    // Logic to find today
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {dd='0'+dd;} if(mm<10) {mm='0'+mm;}
+
+    today = yyyy+'-'+mm+'-'+dd+' 00:00:00';
+
     // assemble objectToSend
-    var objectToSend ={
+    var logInfoToSend = {
       fieldName: 'log_email',
       fieldValue: 'ReachLuis@gmail.com'
     }; //end object to send
     $http({
-      method: 'GET',
+      method: 'POST',
       url: '/getMemberDB',
-      data: objectToSend,
+      data: logInfoToSend
     }).then(function successCallback( response ){
       console.log( 'back from post:', response );
       // set relevant part of response to currentUser
       $scope.currentUser = response.data[0];
+      var dbUser = $scope.currentUser;
       // check to see if a preferred name exists
-      if (!$scope.currentUser.pref_name){
+      if (!dbUser.pref_name){
         // if not, set it equal to first_name
-        $scope.currentUser.pref_name = $scope.currentUser.first_name;
+        $scope.currentUser.pref_name = dbUser.first_name;
       }
-      console.log($scope.currentUser);
-    });
+      console.log('currentUser:',$scope.currentUser);
+      // if user has ever taken a step
+      if (dbUser.step_id){
+        // check for step done today
+        if (dbUser.step_created>today) {
+          $scope.stepDone = true;
+          $scope.currentStep = {id: dbUser.step_id};
+        } else {
+          $scope.stepDone = false;
+        }
+      }
+
+    }); // end http GET call
   }; // end getMember
 
   // if a Member is loggedIn
@@ -41,10 +64,49 @@ myApp.controller('dashController', ['$scope', '$http', function($scope, $http){
 
   //// Prayer Stats Box Code
   $scope.takeStep = function(){
-    $scope.stepDone = true;
+    // First, double-check that a step hasn't been taken today
+    if ($scope.currentStep){
+      console.log('ERROR! Step already taken today!');
+    } else {
+      var stepInfoToSend ={
+        // get the memberID, and send it
+        memberID: $scope.currentUser.member_id,
+        actionType: 1 // Currently Prayer (1) is only action
+      }; //end object to send
+      console.log('stepInfoToSend: ',stepInfoToSend);
+      $http({
+        method: 'POST',
+        url: '/postStepDB',
+        data: stepInfoToSend
+      }).then(function successCallback( response ){
+        console.log( 'back from post:', response );
+        // set relevant part of response to currentStep
+        $scope.currentStep = response.data[0];
+        console.log($scope.currentStep);
+      }); // end http POST call
+      $scope.stepDone = true;
+    }
   };
 
   $scope.undoStep = function(){
-    $scope.stepDone = false;
+    // First, double-check that a step has been taken today
+    if (!$scope.currentStep){
+      console.log('ERROR! Step not taken today!');
+    } else {
+      var stepIDToSend ={
+        // get the stepID, and send it
+        stepID: $scope.currentStep.id,
+      }; //end object to send
+      $http({
+        method: 'DELETE',
+        url: '/deleteStepDB',
+        data: stepIDToSend,
+        headers: {"Content-Type": "application/json;charset=utf-8"}
+      }).then(function successCallback( response ){
+        console.log( 'back from post:', response );
+        $scope.stepDone = false;
+        $scope.currentStep = undefined;
+      }); // end http DELETE call
+    }
   };
 }]);
--- server/app.js
+++ server/app.js
@@ -18,124 +18,127 @@ app.listen(portDecision, function(){
 // setup 'public' as a static resource
 app.use(express.static('public'));
 
-// GET route with a basic response
-app.get('/getRoute', function(req,res){
-  // Code for basic get response
-  console.log( '/getRoute hit' );
-  var responseObject ={
-    response: 'response text from get'
-  };
-  res.send( responseObject );
-}); // end app.get getRoute
+// // GET route with a basic response
+// app.get('/getRoute', function(req,res){
+//   // Code for basic get response
+//   console.log( '/getRoute hit' );
+//   var responseObject ={
+//     response: 'response text from get'
+//   };
+//   res.send( responseObject );
+// }); // end app.get getRoute
 
-// GET route with a Database call
-app.get('/getMemberDB', function(req,res){
-  console.log( '/getMemberDB hit' );
-  var memberSearch = {
-    fieldName: 'log_email',
-    fieldValue: 'ReachLuis@gmail.com'};
-  pg.connect(connectionString, function(err, client, done){
-    if (err){
-      if (verbose) {console.log(err);}
-    } else {
-      if (verbose) {console.log('app.get/getMemberDB connected');}
+// POST route with a Database call
+app.post('/getMemberDB', function(req,res){
+  console.log( '/getMemberDB hit with:',req.body );
 
-      /////// --------- Use to get info from Database --------- ///////
-      var resultsArray=[];
-      //// --------NEED TO EDIT SQL Query
-      var queryResults=client.query('SELECT * FROM members WHERE '+
-        memberSearch.fieldName+'=\''+memberSearch.fieldValue+'\';');
-      queryResults.on('row',function(row){
-        resultsArray.push(row);
-      });
-      queryResults.on('end',function(){
-        if (verbose) {console.log('resultsArray from getRouteDB query:',resultsArray);}
+  var memberSearch = req.body;
+    pg.connect(connectionString, function(err, client, done){
+      if (err){
+        if (verbose) {console.log(err);}
+      } else {
+        if (verbose) {console.log('app.post/getMemberDB connected');}
+
+        var resultsArray=[];
+        var queryResults=client.query('SELECT * FROM members_join_steps '+
+        'WHERE '+memberSearch.fieldName+'=\''+memberSearch.fieldValue+'\''+
+        ' ORDER BY step_id DESC LIMIT 1;');
+        queryResults.on('row',function(row){
+          resultsArray.push(row);
+        });
+        queryResults.on('end',function(){
+          if (verbose) {console.log('resultsArray from query:',resultsArray);}
+          done();
+          return res.send(resultsArray);
+        }); // end queryResults.on('end')
+      }// end else
+    }); // end pg.connect
+  }); // end app.post getRoute
+
+
+  // // post route to receive information from client
+  // app.post('/postRoute', function(req,res){
+  //   if (verbose) {console.log('Route /postRoute hit', req.body);}
+  //   res.send('/postRoute response. Received: '+req.body);
+  // });
+
+  //post route to add information in Database
+  app.post( '/postStepDB', function( req, res ){
+    if (verbose) {console.log( '/postStepDB route hit', req.body );}
+    var stepReport = req.body;
+    var queryString = 'INSERT INTO steps (member_id, action_id) VALUES (($1),($2)) RETURNING id;';
+    if (verbose) {console.log('sending to database:', queryString);}
+    //send queryString to database
+    pg.connect(connectionString, function(err, client, done){
+      if (err){
+        if (verbose) {console.log(err);}
+      }
+      else{
+        if (verbose) {console.log('app.post/postStepDB connected');}
+        var resultsArray=[];
+        var queryResults=client.query(queryString, [stepReport.memberID, stepReport.actionType]);
+        queryResults.on('row',function(row){
+          resultsArray.push(row);
+        });
+        queryResults.on('end',function(){
+          if (verbose) {console.log('resultsArray from getRouteDB query:',resultsArray);}
+          done();
+          return res.send(resultsArray);
+        }); // end queryResults.on('end')
+      }
+    });
+  });//end /postStepDB Route
+
+  // //put route to update information in Database
+  // app.put( '/putRouteDB', function( req, res ){
+  //   if (verbose) {console.log( '/putRouteDB route hit', req.body );}
+  //   //// --------NEED TO ENTER ***: SQL Query
+  //   var queryString = 'UPDATE <table> SET <field> = '+variable+' WHERE <field> = ($1);';
+  //   if (verbose) {console.log('sending to database:', queryString);}
+  //   //send queryString to database
+  //   pg.connect(connectionString, function(err, client, done){
+  //     if (err){
+  //       if (verbose) {console.log(err);}
+  //     }
+  //     else{
+  //       if (verbose) {console.log('app.put/putRouteDB connected');}
+  //       client.query(queryString,[variableToReplaceBling]);
+  //       done();
+  //       return res.sendStatus(200);
+  //     }
+  //   });
+  // });//end /putRoute
+
+  //delete route to update information in Database
+  app.delete( '/deleteStepDB', function( req, res ){
+    if (verbose) {console.log( '/deleteStepDB route hit', req.body );}
+    var stepToDelete = req.body.stepID;
+    var queryString = 'DELETE FROM steps WHERE id = ($1);';
+    if (verbose) {console.log('sending to database:', queryString);}
+    //send queryString to database
+    pg.connect(connectionString, function(err, client, done){
+      if (err){
+        if (verbose) {console.log(err);}
+      }
+      else{
+        if (verbose) {console.log('app.delete/deleteStepDB connected');}
+        client.query(queryString,[stepToDelete]);
         done();
-        return res.send(resultsArray);
-      }); // end queryResults.on('end')
-      /////// --------- Use to get info from Database --------- ///////
+        return res.sendStatus(200);
+      }
+    });
+  });//end /deleteStepDB
 
-    }// end else
-  }); // end pg.connect
-}); // end app.get getRoute
+  // setting catch all route
+  app.get('/*', function(req,res){
+    if (verbose) {console.log('Made it to the catch all route, with',req.params);}
+    var file = req.params[0];
 
-
-// post route to receive information from client
-app.post('/postRoute', function(req,res){
-  if (verbose) {console.log('Route /postRoute hit', req.body);}
-  res.send('/postRoute response. Received: '+req.body);
-});
-
-//post route to add information in Database
-app.post( '/postRouteDB', function( req, res ){
-  if (verbose) {console.log( '/postRouteDB route hit', req.body );}
-  //// --------NEED TO ENTER ***: SQL Query
-  var queryString = 'INSERT INTO <table> (<field1>, <field2>) VALUES (($1),($2));';
-  if (verbose) {console.log('sending to database:', queryString);}
-  //send queryString to database
-  pg.connect(connectionString, function(err, client, done){
-    if (err){
-      if (verbose) {console.log(err);}
+    // checking for valid url
+    if (!file.includes('.')){
+      file = 'views/index.html';
+      // leave params untouched so that NG-routing can still use it
     }
-    else{
-      if (verbose) {console.log('app.post/postRouteDB connected');}
-      client.query(queryString,[variableToReplaceBling]);
-      done();
-      return res.sendStatus(200);
-    }
+
+    res.sendFile(path.resolve('public/', file));
   });
-});//end /postRoute
-
-//put route to update information in Database
-app.put( '/putRouteDB', function( req, res ){
-  if (verbose) {console.log( '/putRouteDB route hit', req.body );}
-  //// --------NEED TO ENTER ***: SQL Query
-  var queryString = 'UPDATE <table> SET <field> = '+variable+' WHERE <field> = ($1);';
-  if (verbose) {console.log('sending to database:', queryString);}
-  //send queryString to database
-  pg.connect(connectionString, function(err, client, done){
-    if (err){
-      if (verbose) {console.log(err);}
-    }
-    else{
-      if (verbose) {console.log('app.put/putRouteDB connected');}
-      client.query(queryString,[variableToReplaceBling]);
-      done();
-      return res.sendStatus(200);
-    }
-  });
-});//end /putRoute
-
-//delete route to update information in Database
-app.delete( '/deleteRouteDB', function( req, res ){
-  if (verbose) {console.log( '/deleteRouteDB route hit', req.body );}
-  //// --------NEED TO ENIT: SQL Query
-  var queryString = 'DELETE FROM <table> WHERE <field> = ($1);';
-  if (verbose) {console.log('sending to database:', queryString);}
-  //send queryString to database
-  pg.connect(connectionString, function(err, client, done){
-    if (err){
-      if (verbose) {console.log(err);}
-    }
-    else{
-      if (verbose) {console.log('app.delete/deleteRouteDB connected');}
-      client.query(queryString,[variableToReplaceBling]);
-      done();
-      return res.sendStatus(200);
-    }
-  });
-});//end /deleteRoute
-
-// setting catch all route
-app.get('/*', function(req,res){
-  if (verbose) {console.log('Made it to the catch all route, with',req.params);}
-  var file = req.params[0];
-
-  // checking for valid url
-  if (!file.includes('.')){
-    file = 'views/index.html';
-    // leave params untouched so that NG-routing can still use it
-  }
-
-  res.sendFile(path.resolve('public/', file));
-});

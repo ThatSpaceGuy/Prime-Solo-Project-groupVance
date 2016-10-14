5e3e9f228cd259e02b94ce7581de3b11a2fba6b7
Luis H. Matos <ReachLuis@Gmail.com>
2016-10-13 22:02:14 -0500

    Group Stats Grid content figured out

--- public/scripts/controllers/dashController.js
+++ public/scripts/controllers/dashController.js
@@ -11,8 +11,11 @@ myApp.controller('dashController', ['$scope', '$http', function($scope, $http){
   $scope.getMember = function(){
     console.log( 'in getMember()' );
 
-    // Logic to find lastDay
-    lastDay = moment().subtract(1, 'days').endOf('day').format();
+    // Logic to find lastDay and last week
+    var currentTime = moment();
+    var lastDay = moment(currentTime).subtract(1, 'days').endOf('day');
+    var lastWeek = lastDay.subtract(6, 'days');
+    console.log('currentTime:',currentTime,'lastDay:',lastDay,'lastWeek:',lastWeek);
     // assemble objectToSend
     var logInfoToSend = {
       memberValue: 'Lui.Matos@gmail.com'
@@ -23,10 +26,11 @@ myApp.controller('dashController', ['$scope', '$http', function($scope, $http){
       data: logInfoToSend
     }).then(function successCallback( response ){
       console.log( 'back from post:', response );
-      // set latest Step information to currentUser
+      // store all group data
       var dbGroup = response.data;
-      $scope.currentUser = dbGroup[0];
-      var dbUser = $scope.currentUser;
+      // set latest Step information to currentUser
+      var dbUser = dbGroup[0];
+      $scope.currentUser = dbUser;
       // check to see if a preferred name exists
       if (!dbUser.pref_name){
         // if not, set it equal to first_name
@@ -36,25 +40,97 @@ myApp.controller('dashController', ['$scope', '$http', function($scope, $http){
       //// Set initial step info for user
       // if user has ever taken a step
       if (dbUser.step_id){
-        // count the number of steps taken by currentUser
-        for (var i = 0; i < dbGroup.length; i++) {
-          if(dbGroup[i].log_email === dbUser.log_email){
-            $scope.userSteps++;
-          } // end if incrementer
-        } // end counting for loop
-
         // check if last step was taken today
-        if (moment(lastDay).isBefore(dbUser.step_created)) {
+        if (moment(dbUser.step_created).isAfter(lastDay)) {
           // then a step was taken today
           $scope.stepDone = true;
           // capture id of last step to be able to delete it
           $scope.currentStep = {id: dbUser.step_id};
         }
       }
-      console.log('num steps:', $scope.userSteps);
       //// Set group info for user
-      $scope.currentGroup = {data: dbGroup};
+      // initialize variables
+      var groupData = {};
+      var gridData = [{}];
+      // loop over every record in dbGroup - all data returned from DB
+      for (var j = 0; j < dbGroup.length; j++) {
+        // Grabbing info for DRYness
+        memberEmail = dbGroup[j].log_email;
+        memberStep = dbGroup[j].step_created;
+        memberName = dbGroup[j].pref_name;
 
+        if (!groupData[memberEmail]){
+          // if ther is no entry yet, then initialize everything for that member
+          groupData[memberEmail]={};
+          groupData[memberEmail].numSteps = 0;
+          groupData[memberEmail].stepArray = [];
+          if (!memberName){
+            groupData[memberEmail].prefName = dbGroup[j].first_name;
+          } else {
+            groupData[memberEmail].prefName = memberName;
+          }
+        }
+        // count this step
+        groupData[memberEmail].numSteps++;
+
+        // check if step was taken within the past week
+        if (moment(memberStep).isAfter(lastWeek)) {
+          // if so, then keep track of it for display
+          groupData[memberEmail].stepArray.push(memberStep);
+        }
+      }
+      console.log('groupData:',groupData);
+      // Now that the group data is ready, build gridData
+      // initialize variables
+      var dayOne = lastWeek.add(1,'days').format('dd');
+      var searchDays = [dayOne];
+      // build searchDays array
+      for (var k = 1; k < 7; k++) {
+        searchDays[k]=moment(searchDays[k-1],'dd',true).add(1,'days').format('dd');
+      }
+      console.log('searchDays:',searchDays);
+      var memberIndex = -1;
+      var dayOfStep;
+      var stepsToCheck;
+      // loop over ever property in groupData
+      for (var groupMember in groupData) {
+        // if the property is actually a groupMember
+        if (groupData.hasOwnProperty(groupMember)) {
+          console.log('found:',groupMember);
+          memberIndex++; // Prepare the memberIndex
+          console.log('memberIndex',memberIndex);
+          gridData[memberIndex]={};
+          // set prefName for Member column
+          gridData[memberIndex].Member = groupData[groupMember].prefName;
+          stepsToCheck = groupData[groupMember].stepArray; // grab for DRYness
+          // loop over every day in the last week
+          for (var l = 0; l < searchDays.length; l++) {
+            // grab the weekday for the column name
+            dateCol = searchDays[l];
+            console.log('dateCol:',dateCol);
+            // initialize empty value for dateCol - overridden if appropriate
+            gridData[memberIndex][dateCol] = '';
+            // loop over every step the member took in the last week
+            for (var i = 0; i < stepsToCheck.length; i++) {
+              // grab the weekday that the step was taken
+              console.log('stepsToCheck:',stepsToCheck,'i:',i);
+              dayOfStep = moment(stepsToCheck[i]).format('dd');
+              console.log('dayOfStep:',dayOfStep);
+              // if it matches the column name
+              if (dayOfStep === dateCol){
+                // then make an X in that column
+                gridData[memberIndex][dateCol] = 'X';
+              }
+            }
+          }
+        }
+      }
+      console.log(gridData);
+      // assign the gridData to be displayed by ui-grid
+      $scope.currentGroup = {data: gridData};
+      // grab steps of currentUser for personal stats box
+      $scope.userSteps = groupData[dbUser.log_email].numSteps;
+      console.log('num steps:', $scope.userSteps);
     }); // end http POST call
   }; // end getMember
 
